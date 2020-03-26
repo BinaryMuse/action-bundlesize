74b10bcd4b003d960f586d313b73eb44207b0099
Michelle Tilley <michelle@michelletilley.net>
2020-03-26 13:26:42 -0700

    Initial test

---
+++ get_stats.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const path = require('path')
+const zlib = require('zlib')
+const exec = require('@actions/exec')
+
+module.exports = async function getStats(subDir) {
+  const workspaceDir = process.env.GITHUB_WORKSPACE
+  const workingDir = path.join(workspaceDir, subDir)
+  const pj = JSON.parse(fs.readFileSync(path.join(workingDir, 'package.json')))
+
+  const config = pj.actionBundlesize
+  if (!config) {
+    throw new Error(`No actionBundlesize config found in package.json in ${subDir}`)
+  }
+
+  await exec.exec(config.build, { cwd: workingDir, })
+
+  const statsPromises = config.files.map(async ({path, name}) => {
+    const filepath = path.join(workingDir, path)
+    const normalSize = getFileSizeInBytes(filepath)
+    const gzippedSize = await getGzippedSizeInBytes(filepath)
+
+    return {
+      path,
+      name,
+      size: {
+        normal: normalSize,
+        gzipped: gzippedSize
+      }
+    }
+  })
+
+  const stats = await Promise.all(statsPromises)
+
+  return stats.reduce((acc, stat) => {
+    acc[stat.path] = stat
+    return acc
+  }, {})
+}
+
+function getFileSizeInBytes(pathToFile) {
+  const fstats = fs.fstatSync(pathToFile)
+  return fstats.size
+}
+
+async function getGzippedSizeInBytes(pathToFile) {
+  const outFile = pathToFile + '.gz'
+  const gzip = zlib.createGzip()
+  const inp = fs.createReadStream(pathToFile)
+  const out = fs.createWriteStream(outFile, { emitClose: true })
+
+  return new Promise((res, rej) => {
+    inp.pipe(gzip)
+      .pipe(out)
+      .on('error', rej)
+      .on('close', () => {
+        const size = getFileSizeInBytes(outFile)
+        res(size)
+      })
+  })
+}
--- index.js
+++ index.js
@@ -1,19 +1,52 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require('@actions/core')
+const github = require('@actions/github')
+const getStats = require('./get_stats')
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const ghToken = core.getInput('github_token')
+    const octokit = new github.GitHub(ghToken)
 
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    const oldStats = await getStats('old')
+    const newStats = await getStats('new')
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
+    const comparisons = Object.keys(newStats).map(path => {
+      const next = oldStats[path]
+      const prev = newStats[path] || { path, name: next.name, size: { normal: 0, gzipped: 0} }
+      const change = {
+        normal: next.size.normal - prev.size.normal,
+        gzipped: next.size.gzipped - prev.size.gzipped
+      }
+
+      return {
+        path,
+        name: next.name,
+        old: prev.size,
+        new: next.size,
+        change
+      }
+    })
+
+    for (const comparison of comparisons) {
+      const name = `Bundlesize: ${comparison.name} (${comparison.path})`
+      // const signNormal = comparison.change.normal >= 0 ? '+' : ''
+      // const signGzipped = comparison.change.gzipped >= 0 ? '+' : ''
+
+      const [repoOwner, repoName] = process.env.GITHUB_REPOSITORY.split('/')
+
+      await octokit.checks.create({
+        owner: repoOwner,
+        repo: repoName,
+        name: name,
+        head_sha: process.env.GITHUB_SHA,
+        conclusion: 'success',
+        output: {
+          title: name,
+          summary: `${comparison.old.normal} bytes -> ${comparison.new.normal} bytes (${comparison.old.gzppped} bytes -> ${comparison.new.gzipped} bytes gzipped)`
+        }
+      })
+    }
+  }
   catch (error) {
     core.setFailed(error.message);
   }
